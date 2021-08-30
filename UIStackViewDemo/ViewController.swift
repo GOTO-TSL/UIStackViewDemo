d8182256a9eb21777a0d942b72720aad11b6f4e5
GOTO-TSL <kosuke.iosappbuilding@gmail.com>
2021-08-31 01:46:44 +0900

    stackViewを表示し，比で分ける関数を作成しました．

--- UIStackViewDemo/ViewController.swift
+++ UIStackViewDemo/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let topView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.accessibilityIdentifier = "top"
+        return view
+    }()
+    
+    let middleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.accessibilityIdentifier = "middle"
+        return view
+    }()
+    
+    let bottomView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.accessibilityIdentifier = "bottom"
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+        
+        let stack = UIStackView(arrangedSubviews: [topView, middleView, bottomView])
+        stack.distribution = .fill
+        stack.axis = .vertical
+        stack.alignment = .fill
+        stack.accessibilityIdentifier = "stack"
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stack)
+        
+        [
+            stack.topAnchor.constraint(equalTo: view.topAnchor),
+            stack.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stack.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stack.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ].forEach { $0.isActive = true }
+        
+        stack.divide(parentHeight: stack.heightAnchor, by: [3,1,6])
+        
+        
     }
+    
+}
 
-
+extension UIStackView {
+    func divide(parentHeight: NSLayoutDimension, by ratio: [CGFloat]) {
+        translatesAutoresizingMaskIntoConstraints = false
+        for (view, element) in zip(arrangedSubviews, ratio) {
+            view.heightAnchor.constraint(equalTo: parentHeight,
+                                         multiplier: element/ratio.reduce(0, +)).isActive = true
+        }
+    }
 }
 
