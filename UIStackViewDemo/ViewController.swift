a7c4686e2863f2d02f3d5a36da24fffd1769ec94
GOTO-TSL <kosuke.iosappbuilding@gmail.com>
2021-08-30 22:11:21 +0900

    Initial Commit

---
+++ UIStackViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIStackViewDemo
+//
+//  Created by 後藤孝輔 on 2021/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
